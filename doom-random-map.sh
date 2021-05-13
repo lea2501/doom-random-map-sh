e6065bd5b9bb4afcd56ce08cd6b7671f2329cd5f
Lea Chescotta <ochescotta@teco.com.ar>
2021-05-13 20:44:48 -0300

    Updated parameters and added heretic and hexen support

--- doom-random-map.sh
+++ doom-random-map.sh
@@ -1,39 +1,70 @@
-#!/bin/sh
+#!/bin/bash
 
-# exit when any command fails
-#set -e
+# fail if any commands fails
+set -e
+# debug log
+#set -x
+
+Help() {
+  # Display Help
+  echo "Runs automation test locally sending results to Jenkins."
+  echo ""
+  echo "  Syntax: ./doom-random-map.sh [params]"
+  echo ""
+  echo "  Available parameters:"
+  echo "    doom game          -> doom | doom2 | tnt | plutonia | heretic | hexen"
+  echo "    engine             -> chocolate | crispy | prboom-plus | gzdoom"
+  echo "    map limit          -> vanilla | limit-removing | boom | zdoom | slige | oblige"
+  echo ""
+}
+
+if [[ $1 == "--help" || $1 == "-h" ]]; then
+    Help
+    exit 1
+fi
+if [ -z "$1" ]; then
+  Help
+  exit 1
+fi
+if [ -z "$2" ]; then
+  Help
+  exit 1
+fi
+
+### check parameter values
+doom_game=(doom doom2 tnt plutonia heretic hexen)
+if [[ " "${doom_game[@]}" " != *" $1 "* ]]; then
+  echo "$1: not recognized. Valid doom games are:"
+  echo "${doom_game[@]/%/,}"
+  exit 1
+fi
+engine=(chocolate crispy prboom-plus gzdoom)
+if [[ " "${engine[@]}" " != *" $2 "* ]]; then
+  echo "$2: not recognized. Valid engines are:"
+  echo "${engine[@]/%/,}"
+  exit 1
+fi
+#map_limit=(vanilla limit-removing boom zdoom)
+#if [[ " "${map_limit[@]}" " != *" $3 "* ]]; then
+#  echo "$3: not recognized. Valid map limits are:"
+#  echo "${map_limit[@]/%/,}"
+#  exit 1
+#fi
 
 ### Configuration
 GAME_DIR=~/games/doom
-IWADS_DIR=$GAME_DIR/wads/original
-USAGE_MESSAGE="Usage: doom-random-map.sh <doom|doom2|tnt|plutonia> <chocolate-doom|crispy-doom|prboom-plus|gzdoom> <vanilla|limit_removing|boom|zdoom>(Optional)"
-if [[ $1 == "--help" || $1 == "-h" ]]; then
-    echo $USAGE_MESSAGE
-    exit 1
-fi
-if [[ -z $1 ]]; then
-    echo $USAGE_MESSAGE
-    exit 1
-else
-    IWAD=$1
-fi
-if [[ -z $2 ]]; then
-    echo $USAGE_MESSAGE
-    exit 1
-else
-    ENGINE=$2
-fi
-if [[ ! -z $3 ]]; then
-    echo $PWAD_DIR
-fi
+IWADS_DIR=$GAME_DIR/wads/iwads
+IWAD=$1
+ENGINE=$2
+MAP_LIMIT=$3
 
 ### Script
 get_map_file() {
-    if [[ ! -z $PWAD_DIR ]]; then
-        pwadfile=$(find $GAME_DIR/wads/$IWAD/$PWAD_DIR/*/*.wad ! -name *tex*.* ! -name *res*.* ! -name *fix.* ! -name *demo*.* -type f 2>/dev/null | shuf -n 1)
-    elif [[ $ENGINE == "chocolate-doom" ]]; then
+    if [[ ! -z $MAP_LIMIT ]]; then
+        pwadfile=$(find $GAME_DIR/wads/$IWAD/$MAP_LIMIT/*/*.wad ! -name *tex*.* ! -name *res*.* ! -name *fix.* ! -name *demo*.* -type f 2>/dev/null | shuf -n 1)
+    elif [[ $ENGINE == "chocolate" ]]; then
         pwadfile=$(find $GAME_DIR/wads/$IWAD/vanilla/*/*.wad ! -name *tex*.* ! -name *res*.* ! -name *fix.* ! -name *demo*.* -type f 2>/dev/null | shuf -n 1)
-    elif [[ $ENGINE == "crispy-doom" ]]; then
+    elif [[ $ENGINE == "crispy" ]]; then
         pwadfile=$(find $GAME_DIR/wads/$IWAD/{vanilla,limit_removing}/*/*.wad ! -name *tex*.* ! -name *res*.* ! -name *fix.* ! -name *demo*.* -type f 2>/dev/null | shuf -n 1)
     elif [[ $ENGINE == "prboom-plus" ]]; then
         pwadfile=$(find $GAME_DIR/wads/$IWAD/{vanilla,limit_removing,boom}/*/*.wad ! -name *tex*.* ! -name *res*.* ! -name *fix.* ! -name *demo*.* -type f 2>/dev/null | shuf -n 1)
@@ -59,7 +90,7 @@ get_map_file() {
     echo "PWAD name: $pwadfilename"
 
     # Get maps from pwad
-    gzdoom -iwad $IWADS_DIR/$IWAD.wad -file $pwadfile -norun -hashfiles > /dev/null
+    gzdoom -iwad $IWADS_DIR/$IWAD.wad -file $pwadfile -norun -hashfiles > /dev/null || true
 
     if [[ $IWAD == "doom2" ]]; then
         pwadmap=$(cat fileinfo.txt | grep $pwadfilename | grep " MAP" | awk '{print $4}' | sed -e "s/^MAP//" -e 's/,//g' | shuf -n 1)
@@ -70,6 +101,12 @@ get_map_file() {
 
         mapnumbercheck=$(echo "$pwadmap" | awk '$0 ~/[^0-9]/ { print "NOT_NUMBER" }')
     elif [[ $IWAD == "doom" ]]; then
+        pwadmap=$(cat fileinfo.txt | grep $pwadfilename | grep -E " E[1-5]M" | awk '{print $4}' | shuf -n 1 | sed -r 's/[EM]+/ /g' | sed -e "s/^0//" -e 's/,//g')
+        echo "PWAD map number: $pwadmap"
+    elif [[ $IWAD == "heretic" ]]; then
+        pwadmap=$(cat fileinfo.txt | grep $pwadfilename | grep -E " E[1-5]M" | awk '{print $4}' | shuf -n 1 | sed -r 's/[EM]+/ /g' | sed -e "s/^0//" -e 's/,//g')
+        echo "PWAD map number: $pwadmap"
+    elif [[ $IWAD == "hexen" ]]; then
         pwadmap=$(cat fileinfo.txt | grep $pwadfilename | grep -E " E[1-5]M" | awk '{print $4}' | shuf -n 1 | sed -r 's/[EM]+/ /g' | sed -e "s/^0//" -e 's/,//g')
         echo "PWAD map number: $pwadmap"
     fi
@@ -93,19 +130,64 @@ done
 
 # You can have separate mods "sets" for the source ports
 if [[ $ENGINE == "gzdoom" ]]; then
-    MODS="$GAME_DIR/mods/vanilla/pk_doom_sfx/pk_doom_sfx_20120224.wad $GAME_DIR/mods/vanilla/jovian_palette/JovPal.wad $GAME_DIR/mods/vanilla/smoothed/smoothed.wad $GAME_DIR/mods/zdoom/vanilla_essence/vanilla_essence_4_3.pk3"
-elif [[ $ENGINE == "chocolate-doom" ]]; then
-    MODS="$GAME_DIR/mods/vanilla/pk_doom_sfx/pk_doom_sfx_20120224.wad $GAME_DIR/mods/vanilla/jovian_palette/JovPal.wad"
+    if [[ "$IWAD" == *"doom"* || "$IWAD" == "tnt" || "$IWAD" == "plutonia" ]]; then
+        #MODS="$GAME_DIR/mods/vanilla/pk_doom_sfx/pk_doom_sfx_20120224.wad $GAME_DIR/mods/vanilla/jovian_palette/JovPal.wad $GAME_DIR/mods/zdoom/vanilla_essence/vanilla_essence_4_3.pk3"
+        #MODS="$GAME_DIR/mods/vanilla/pk_doom_sfx/pk_doom_sfx_20120224.wad $GAME_DIR/mods/vanilla/jovian_palette/JovPal.wad $GAME_DIR/mods/zdoom/vanilla_essence/vanilla_essence_4_3.pk3"
+        MODS="$GAME_DIR/mods/vanilla/pk_doom_sfx/pk_doom_sfx_20120224.wad $GAME_DIR/mods/vanilla/dimm_pal/doom-pal.wad $GAME_DIR/mods/zdoom/vanilla_essence/vanilla_essence_4_3.pk3"
+    elif [[ "$IWAD" == "heretic" ]]; then
+        #MODS="$GAME_DIR/mods/vanilla/pk_doom_sfx/pk_doom_sfx_20120224.wad $GAME_DIR/mods/vanilla/jovian_palette/JovPal.wad $GAME_DIR/mods/zdoom/vanilla_essence/vanilla_essence_4_3.pk3"
+        MODS="$GAME_DIR/mods/vanilla/dimm_pal/her-pal.wad $GAME_DIR/mods/zdoom/brutal/brutal_heretic/Heretic-Shadow_Collection/1_BRUTAL_HERETIC/BrutalHereticRPG_V2.2.pk3"
+    elif [[ "$IWAD" == "hexen" ]]; then
+        #MODS="$GAME_DIR/mods/vanilla/pk_doom_sfx/pk_doom_sfx_20120224.wad $GAME_DIR/mods/vanilla/jovian_palette/JovPal.wad $GAME_DIR/mods/zdoom/vanilla_essence/vanilla_essence_4_3.pk3"
+        MODS="$GAME_DIR/mods/vanilla/dimm_pal/hex-pal.wad $GAME_DIR/mods/zdoom/vanilla_essence/vanilla_essence_4_3.pk3"
+    fi
+    #MODS="$GAME_DIR/mods/vanilla/pk_doom_sfx/pk_doom_sfx_20120224.wad $GAME_DIR/mods/vanilla/jovian_palette/JovPal.wad $GAME_DIR/mods/vanilla/smoothed/smoothed.wad $GAME_DIR/mods/zdoom/vanilla_essence/vanilla_essence_4_3.pk3"
+elif [[ $ENGINE == "chocolate" ]]; then
+    if [[ "$IWAD" == *"doom"* || "$IWAD" == "tnt" || "$IWAD" == "plutonia" ]]; then
+        #MODS="$GAME_DIR/mods/vanilla/pk_doom_sfx/pk_doom_sfx_20120224.wad $GAME_DIR/mods/vanilla/jovian_palette/JovPal.wad"
+        MODS="$GAME_DIR/mods/vanilla/pk_doom_sfx/pk_doom_sfx_20120224.wad $GAME_DIR/mods/vanilla/dimm_pal/doom-pal.wad"
+    elif [[ "$IWAD" == "heretic" ]]; then
+        MODS="$GAME_DIR/mods/vanilla/dimm_pal/her-pal.wad"
+    elif [[ "$IWAD" == "hexen" ]]; then
+        MODS="$GAME_DIR/mods/vanilla/dimm_pal/hex-pal.wad"
+    fi
+elif [[ $ENGINE == "crispy" ]]; then
+    if [[ "$IWAD" == *"doom"* || "$IWAD" == "tnt" || "$IWAD" == "plutonia" ]]; then
+        #MODS="$GAME_DIR/mods/vanilla/pk_doom_sfx/pk_doom_sfx_20120224.wad $GAME_DIR/mods/vanilla/jovian_palette/JovPal.wad"
+        MODS="$GAME_DIR/mods/vanilla/pk_doom_sfx/pk_doom_sfx_20120224.wad $GAME_DIR/mods/vanilla/dimm_pal/doom-pal.wad"
+    elif [[ "$IWAD" == "heretic" ]]; then
+        MODS="$GAME_DIR/mods/vanilla/dimm_pal/her-pal.wad"
+    elif [[ "$IWAD" == "hexen" ]]; then
+        MODS="$GAME_DIR/mods/vanilla/dimm_pal/hex-pal.wad"
+    fi
+elif [[ $ENGINE == "prboom-plus" ]]; then
+    #MODS="$GAME_DIR/mods/vanilla/pk_doom_sfx/pk_doom_sfx_20120224.wad $GAME_DIR/mods/vanilla/jovian_palette/JovPal.wad"
+    MODS="$GAME_DIR/mods/vanilla/pk_doom_sfx/pk_doom_sfx_20120224.wad $GAME_DIR/mods/vanilla/dimm_pal/doom-pal.wad"
 else
-    MODS="$GAME_DIR/mods/vanilla/pk_doom_sfx/pk_doom_sfx_20120224.wad $GAME_DIR/mods/vanilla/jovian_palette/JovPal.wad $GAME_DIR/mods/vanilla/smoothed/smoothed.wad"
+    #MODS="$GAME_DIR/mods/vanilla/pk_doom_sfx/pk_doom_sfx_20120224.wad $GAME_DIR/mods/vanilla/jovian_palette/JovPal.wad $GAME_DIR/mods/vanilla/smoothed/smoothed.wad"
+    #MODS="$GAME_DIR/mods/vanilla/pk_doom_sfx/pk_doom_sfx_20120224.wad $GAME_DIR/mods/vanilla/dimm_pal/doom-pal.wad $GAME_DIR/mods/vanilla/smoothed/smoothed.wad"
+    MODS="$GAME_DIR/mods/vanilla/pk_doom_sfx/pk_doom_sfx_20120224.wad $GAME_DIR/mods/vanilla/dimm_pal/doom-pal.wad"
 fi
 
-if [[ $ENGINE == "chocolate-doom" ]]; then
-    commandline="chocolate-doom -fullscreen -iwad $IWADS_DIR/$IWAD.wad -merge $pwadfile $MODS -savedir $GAME_DIR/savegames/$IWAD/ -skill 3 -warp $pwadmap"
-elif [[ $ENGINE == "crispy-doom" ]]; then
-    commandline="crispy-doom -fullscreen -iwad $IWADS_DIR/$IWAD.wad -file $pwadfile $MODS -savedir $GAME_DIR/savegames/$IWAD/ -skill 3 -warp $pwadmap"
+if [[ $ENGINE == "chocolate" ]]; then
+    if [[ "$IWAD" == *"doom"* || "$IWAD" == "tnt" || "$IWAD" == "plutonia" ]]; then
+        commandline="chocolate-doom -fullscreen -iwad $IWADS_DIR/$IWAD.wad -merge $pwadfile $MODS -savedir $GAME_DIR/savegames/$IWAD/ -skill 3 -warp $pwadmap"
+    elif [[ "$IWAD" == "heretic" ]]; then
+        commandline="chocolate-heretic -fullscreen -iwad $IWADS_DIR/$IWAD.wad -merge $pwadfile $MODS -savedir $GAME_DIR/savegames/$IWAD/ -skill 3 -warp $pwadmap"
+    elif [[ "$IWAD" == "hexen" ]]; then
+        commandline="chocolate-hexen -fullscreen -iwad $IWADS_DIR/$IWAD.wad -merge $pwadfile $MODS -savedir $GAME_DIR/savegames/$IWAD/ -skill 3 -warp $pwadmap"
+    fi
+elif [[ $ENGINE == "crispy" ]]; then
+    if [[ "$IWAD" == *"doom"* || "$IWAD" == "tnt" || "$IWAD" == "plutonia" ]]; then
+        commandline="crispy-doom -fullscreen -iwad $IWADS_DIR/$IWAD.wad -file $pwadfile $MODS -savedir $GAME_DIR/savegames/$IWAD/ -skill 3 -warp $pwadmap"
+    elif [[ "$IWAD" == "heretic" ]]; then
+        commandline="crispy-heretic -fullscreen -iwad $IWADS_DIR/$IWAD.wad -file $pwadfile $MODS -savedir $GAME_DIR/savegames/$IWAD/ -skill 3 -warp $pwadmap"
+    elif [[ "$IWAD" == "hexen" ]]; then
+        commandline="crispy-hexen -fullscreen -iwad $IWADS_DIR/$IWAD.wad -file $pwadfile $MODS -savedir $GAME_DIR/savegames/$IWAD/ -skill 3 -warp $pwadmap"
+    fi
 elif [[ $ENGINE == "prboom-plus" ]]; then
-    commandline="prboom-plus -vidmode gl -complevel 17 -width 1920 -height 1080 -fullscreen -geom 640x360f -aspect 16:9 -iwad $IWADS_DIR/$IWAD.wad -file $pwadfile $MODS -save $GAME_DIR/savegames/$IWAD/ -skill 3 -warp $pwadmap"
+    #commandline="prboom-plus -vidmode gl -complevel 17 -width 1920 -height 1080 -fullscreen -geom 640x360f -aspect 16:9 -iwad $IWADS_DIR/$IWAD.wad -file $pwadfile $MODS -save $GAME_DIR/savegames/$IWAD/ -skill 3 -warp $pwadmap"
+    commandline="prboom-plus -vidmode gl -complevel 17 -width 1920 -height 1080 -fullscreen -aspect 16:9 -iwad $IWADS_DIR/$IWAD.wad -file $pwadfile $MODS -save $GAME_DIR/savegames/$IWAD/ -skill 3 -warp $pwadmap"
 elif [[ $ENGINE == "gzdoom" ]]; then
     commandline="gzdoom -width 1920 -height 1080 -fullscreen -iwad $IWADS_DIR/$IWAD.wad -file $pwadfile $MODS -savedir $GAME_DIR/savegames/$IWAD/ -skill 3 -warp $pwadmap"
 fi
